db029ad4976bc794628c7d437e18cd74dcc78716
Satish Parida <satish.2441@gmail.com>
2018-12-28 15:11:04 +0530

    Add files via upload

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,10 @@
 var display_field_clear='Y';
+var user_input_to_text='N';
+var ans=0;
+var result_value=0;
 
 function displayInScreen(data_from_button) {
-    if (display_field_clear=='Y'){
+    if (display_field_clear=='Y' && user_input_to_text=='N'){
         display_String=data_from_button;
         document.getElementById('cal_display_values').value=display_String;
         display_field_clear='N';
@@ -14,20 +17,45 @@ function displayInScreen(data_from_button) {
 }
 
 function calculateResult() {
-    result_value=eval(document.getElementById('cal_display_values').value);
+    try{
+        result_value=eval(document.getElementById('cal_display_values').value);
+    }
+    catch(err){
+        document.getElementById('cal_display_result').value="In-Valid Expression";
+        return;
+    }
     document.getElementById('cal_display_result').value=result_value;
     display_field_clear='Y';
+    document.getElementById('cal_button_answer').value=result_value;/*Not Required*/
+    ans=result_value;
 }
 
 function calculatrClr(){
     document.getElementById('cal_display_values').value="";
     document.getElementById('cal_display_result').value="";
     display_field_clear='N';
+    ans=0;
 }
 
+function calculatrClr_except_Ans(){
+    calculatrClr();
+    ans=result_value;
+}
+
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode==13) calculateResult();
+    if (charCode != 46
+        && charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    user_input_to_text='Y';
+    return true;
+}
+
+
 /* TODO:
-1. Allow user input to text field
-2. Dont allow anything other than numeric and expected symbols
+2. Dont allow anything other than numeric and expected symbols from user input
 3. Validate the expression, if invalid then throw correct error in output window
-4. Accept enter as = from the textbox field
+5. Store Previous Expressions and results
 */
